90af00489989d70374ed29e45c32f310740daca2
Philip Simon <philip.si@protonmail.com>
2025-06-05 21:46:40 +0200

    direct instantiation support for the geometric primitives, added banner

---
+++ objects/__init__.py
@@ -0,0 +1,15 @@
+from box import Box
+from sphere import Sphere
+from plane import Plane
+from transform import Transform
+from cylinder import Cylinder
+from line import Line
+
+__all__ = [
+    "Box",
+    "Sphere",
+    "Plane",
+    "Transform",
+    "Cylinder",
+    "Line"
+]
