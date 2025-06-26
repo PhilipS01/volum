a374b131b7657aadf2e5fc0dcd30148c1d846fe7
Philip Simon <philip.si@protonmail.com>
2025-06-26 14:41:25 +0200

    constants

---
+++ volum/config/constants.py
@@ -0,0 +1,15 @@
+class UIColors:
+    """Class to hold color constants for the UI and materials."""
+    primary = "#4A90E2"  # Primary color for the UI
+    secondary = "#50E3C2"  # Secondary color for the UI
+    accent = "#F5A623"  # Accent color for the UI
+    error = "#D0021B"  # Error color for the UI
+    warning = "#F8E71C"  # Warning color for the UI
+    success = "#7ED321"  # Success color for the UI
+    
+class MaterialColors:
+    """Class to hold color constants for materials."""
+    default = "#5f9ea0"  # Default color for materials
+    metallic = "#C0C0C0"  # Metallic color for materials
+    matte = "#808080"  # Matte color for materials
+    specular = "#FFFFFF"  # Specular color for materials
