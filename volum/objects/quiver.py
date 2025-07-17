193e425c8a2ed7e6dd2486d04f88e3aa7811441f
Philip Simon <philip.si@protonmail.com>
2025-07-18 00:11:38 +0200

    refactored args

--- volum/objects/quiver.py
+++ volum/objects/quiver.py
@@ -7,66 +7,89 @@ from volum.objects.cone import Cone
 class Quiver(SceneObject):
     """Represents a 2D or 3D quiver plot in the 3D scene."""
 
-    _color: str = 'black'  # Default color for the arrows
+    color_schemes = ["viridis", "magma", "plasma", "inferno", "cividis"]
 
-
-    def __init__(self, *comps: Union[np.typing.NDArray[np.float64], List[float]], object: SceneObject = Cone(1,3, 3), material: Union[MeshMaterial, str, None], colormap: Optional[str], **kwargs):
+    def __init__(self, *args: Union[np.typing.NDArray[np.float64], List[float]], object: SceneObject = Cone(.1, .3, radial_segments=12), colormap: Optional[str] = None, **kwargs):
         """Initialize the Quiver.
-
+    
         Args:
             comps: Arrays representing points and vector components.
                 
                 Can be [X, Y, Z, U, V, W] or [X, Y, Z], [U, V, W].
                 
                 X, Y, Z, U, V, W can be arrays of floats.
+
+                Please keep in mind that the second position parameter is the "height" axis in volum.
                 
                 Also supports meshgrid inputs like X, Y = np.meshgrid(x, y).
             object: SceneObject to use for the arrow representation.
-            material: Material for the arrow objects. Set it's color attribute to color scheme like 'viridis' or 'plasma' if using a colormap.
+            material: Material for the arrow objects. Set it's color attribute to color scheme like 'viridis' or 'magma' if using a colormap.
             colormap: Determines how colors are mapped to arrows. 
                 Options are 'magnitude' or 'height'.
         """
 
-        if material is None:
-            material = StandardMaterial()
-
         assert isinstance(object, SceneObject), "Object must be a SceneObject"
-        assert isinstance(material, (MeshMaterial, str)), "Material must be a MeshMaterial or a string"
         assert colormap in [None, 'magnitude', 'height'], "colormap must be either 'magnitude' or 'height' or None"
 
         # Handle input cases
-        if len(comps) == 6:  # [X, Y, Z, U, V, W]
-            X, Y, Z, U, V, W = map(np.asarray, comps)
-        elif len(comps) == 2:  # [X, Y, Z], [U, V, W]
-            pts, vecs = map(np.asarray, comps)
-            if pts.shape[0] != 3 or vecs.shape[0] != 3:
-                raise ValueError("Expected [X, Y, Z], [U, V, W] with each being arrays of floats.")
-            X, Y, Z = pts
-            U, V, W = vecs
+        if len(args) == 6:  # [X, Y, Z, U, V, W]
+            X, Y, Z, U, V, W = map(np.asarray, args)
+            
+            # Validate shapes
+            if not (X.shape == Y.shape == Z.shape == U.shape == V.shape == W.shape):
+                raise ValueError("All input arrays must have the same shape.")
+            
+            # Flatten arrays if they come from meshgrid
+            X, Y, Z, U, V, W = map(np.ravel, (X, Y, Z, U, V, W))
+
+            # Combine points and vectors
+            pts = np.stack((X, Y, Z), axis=-1)
+            vecs = np.stack((U, V, W), axis=-1)
+
+            # Store points and vectors
+            self.points = pts
+            self.vectors = vecs
+
+        elif len(args) == 2:  # already stacked arrays (e.g. from builder)
+            self.points, self.vectors = map(np.asarray, args)
+
+            if self.points.shape[1] != 3 or self.vectors.shape[1] != 3:
+                raise ValueError("Input arrays must have shape (3, N) for points and vectors.")
+            
         else:
-            raise ValueError("Invalid input. Expected [X, Y, Z, U, V, W] or [X, Y, Z], [U, V, W].")
+            raise ValueError("Invalid input. Expected [X, Y, Z, U, V, W] or [[X, Y, Z], [U, V, W]].")
 
-        # Validate shapes
-        if not (X.shape == Y.shape == Z.shape == U.shape == V.shape == W.shape):
-            raise ValueError("All input arrays must have the same shape.")
-
-        # Flatten arrays if they come from meshgrid
-        X, Y, Z, U, V, W = map(np.ravel, (X, Y, Z, U, V, W))
-
-        # Combine points and vectors
-        pts = np.stack((X, Y, Z), axis=-1)
-        vecs = np.stack((U, V, W), axis=-1)
-
-        # Store points and vectors
-        self.points = pts
-        self.vectors = vecs
+       
         self._object = object
         # Other attributes
         self._title = kwargs.pop('title', '')
         self._colormap = colormap
+        self._color = kwargs.pop('color', None)
 
-        super().__init__(material=material, **kwargs)
+        super().__init__(material=None, **kwargs)
 
+
+    @property
+    def color(self) -> str:
+        """Get the color of the quiver arrows."""
+        return self._color
+    
+    @color.setter
+    def color(self, value: str):
+        """Set the color of the quiver arrows.
+        
+        Args:
+            value (str): Color name, hex code or color scheme name.
+        """
+        if not isinstance(value, str):
+            raise TypeError("color must be a string")
+
+        assert isinstance(self.object.material, MeshMaterial), "Quiver target object must have a MeshMaterial"
+
+        if value not in Quiver.color_schemes:
+            self.object.material.color = value
+
+        self._color = value
 
     @property
     def colormap(self) -> Optional[str]:
@@ -108,38 +131,11 @@ class Quiver(SceneObject):
         """Set the title of the quiver plot."""
         self._title = value
 
-    #@classmethod
-    #def from_dict(cls, data: dict) -> "Quiver":
-    #    import matplotlib
-    #    matplotlib.use('Agg')  # Use non-GUI backend before importing pyplot
-    #    import matplotlib.pyplot as plt
-    #    
-    #    fig, ax = plt.subplots()
-    #
-    #    x_data = data.get("x", [])
-    #    y_data = data.get("y", [])
-    #    for x, y in zip(x_data, y_data):
-    #        ax.plot(x, y)
-    #
-    #    meta = data.get("metadata", {}).get("axes", [{}])[0]
-    #    ax.set_title(meta.get("title", ""))
-    #    ax.set_xlabel(meta.get("xlabel", ""))
-    #    ax.set_ylabel(meta.get("ylabel", ""))
-    #    if "xlim" in meta:
-    #        ax.set_xlim(meta["xlim"])
-    #    if "ylim" in meta:
-    #        ax.set_ylim(meta["ylim"])
-    #
-    #    return cls(plot=fig, width=data.get("width", 5), height=data.get("height", 4), double_sided=data.get("double_sided", False))
-
     def to_dict(self):
-        
-
         return {
             "type": "Quiver",
             "object": self.object.to_dict(),
-            "material": self.material.to_dict(),
-            "points": self.points.tolist(),
-            "vectors": self.vectors.tolist(),
+            "args": (self.points.tolist(), self.vectors.tolist()),
             "colormap": self.colormap,
+            "colorscheme": self.color if self.color in Quiver.color_schemes else None,
         }
